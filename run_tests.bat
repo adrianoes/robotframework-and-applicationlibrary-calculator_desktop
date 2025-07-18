6511131a9e927fd0ec1332f9b104890e2df96814
adrianoes <eustaquio.santos.adriano@gmail.com>
2025-07-18 17:18:05 +0200

    fixing reports path in workflow

--- run_tests.bat
+++ run_tests.bat
@@ -1,30 +1,21 @@
 @echo off
-REM Absolute path to WinAppDriver.exe
-set WINAPPDRIVER_PATH="C:\Program Files\Windows Application Driver\WinAppDriver.exe"
-
-REM Absolute path to the Robot Framework project folder (where the 'tests' folder is located)
-set ROBOT_PROJECT_PATH="C:\robotframework-and-applicationlibrary-calculator_desktop"
-
-REM Absolute path to the results folder
-set RESULTS_PATH=%ROBOT_PROJECT_PATH%\results
-
 REM Start WinAppDriver in the background
-start "" %WINAPPDRIVER_PATH%
+start "" "C:\Program Files\Windows Application Driver\WinAppDriver.exe"
 
 REM Wait 5 seconds to ensure WinAppDriver is ready
 timeout /t 5 /nobreak >nul
 
 REM Create the results folder if it does not exist, or clean it if it does
-if not exist %RESULTS_PATH% (
-    mkdir %RESULTS_PATH%
+if not exist results (
+    mkdir results
 ) else (
     echo Cleaning the results folder...
-    rmdir /s /q %RESULTS_PATH%
-    mkdir %RESULTS_PATH%
+    rmdir /s /q results
+    mkdir results
 )
 
 REM Run Robot Framework tests and save reports in the results folder
-robot -d %RESULTS_PATH% tests
+robot -d ./results tests
 
 REM Wait 3 seconds and automatically close WinAppDriver
 timeout /t 3 /nobreak >nul
